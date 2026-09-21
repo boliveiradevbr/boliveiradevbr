d9a0397d1e915a372864e21f21cd51ee071b3afa
Bruno Oliveira <83931779+boliveiradevbr@users.noreply.github.com>
2026-09-21 12:26:28 -0300

    refactor(website): refactor to new style of website

---
+++ js/script.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                mono: [
+                    'JetBrains Mono',
+                    'IBM Plex Mono',
+                    'SFMono-Regular',
+                    'Consolas',
+                    'Liberation Mono',
+                    'monospace'
+                ],
+                sans: [
+                    'Arial',
+                    'Helvetica Neue',
+                    'Helvetica',
+                    'sans-serif'
+                ]
+            },
+            colors: {
+                ink: '#0B0B0B',
+                line: '#222222',
+                silver: '#A3A3A3',
+                signal: '#00FF66'
+            }
+        }
+    }
+}
